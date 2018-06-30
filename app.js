135c7fbcc96ef5026ebe6972124aa0494728560f
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-06-30 15:34:45 -0400

    Add local db

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path         = require('path');
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/movieproject', {useMongoClient: true})
+  .connect(process.env.MONGODB_URI, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
