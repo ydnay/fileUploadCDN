a460128e2488eb3f9421bba4139edb9b5cddc94c
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-06-30 15:16:31 -0400

    Add cloudinary

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
-const router  = express.Router();
+const Movie = require('../models/movie.js');
+const uploadCloud = require('../config/cloudinary.js');
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Movie.find()
+  .then((movies) => {
+    res.render('index', { movies });
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+});
+
+router.get('/movie/add', (req, res, next) => {
+  res.render('movie-add')
+});
+
+router.post('/movie/add', uploadCloud.single('photo'), (req, res, next) => {
+  const { title, description } = req.body;
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+  const newMovie = new Movie({title, description, imgPath, imgName})
+  newMovie.save()
+  .then(movie => {
+    res.redirect('/')
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 module.exports = router;
